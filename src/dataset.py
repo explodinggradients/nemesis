edfeeb93875def78e6c8741cf99a8b4965d9441b
Shahules786 <Shahules786@gmail.com>
2023-05-03 13:01:42 +0530

    add multi-split support

--- src/dataset.py
+++ src/dataset.py
@@ -1,38 +1,35 @@
 from collections import defaultdict
-from re import split
-from sre_parse import SPECIAL_CHARS
-from typing import Any
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 from datasets import load_dataset
-from dataclasses import dataclass
-from transformers import PreTrainedTokenizer
-import torch
-from utils import SPECIAL_TOKENS
 import re
+from typing import Union, List
+from omegaconf import OmegaConf 
 
-@dataclass
 class HFSummary(Dataset):
     name = "openai/summarize_from_feedback"
     
-    def __init__(self, split:str="train"):
+    def __init__(self, split:Union[List[str], str]="train"):
         super().__init__()
-        self.split = split
-        dataset = load_dataset(self.name,"axis",split=split)
+        if isinstance(split,str):
+            split = [split]
+        self.split = OmegaConf.to_object(split)
+        dataset = load_dataset(self.name,"axis",split=self.split)
         self.data_dict = self.prepare_axis(dataset)
         self.postids = list(self.data_dict.keys())
 
     def prepare_axis(self, dataset):
 
         data_dict = defaultdict(dict)
-        for item in dataset:
-            if item["summary"]["axes"].get("overall") is not None:
-                postid = item["info"]["id"]
-                summary = {k:item["summary"][k] for k in ["text","axes"]}
-                if postid not in data_dict.keys():
-                    instruction = "summarize: " + (item["info"]["post"]  or item["info"]["article"])
-                    data_dict[postid].update({"post":instruction,"summaries":[summary]})
-                else:
-                    data_dict[postid]["summaries"].append(summary)
+        for data in dataset:
+            for item in data:
+                if item["summary"]["axes"].get("overall") is not None:
+                    postid = item["info"]["id"]
+                    summary = {k:item["summary"][k] for k in ["text","axes"]}
+                    if postid not in data_dict.keys():
+                        instruction = "summarize: " + (item["info"]["post"]  or item["info"]["article"])
+                        data_dict[postid].update({"post":instruction,"summaries":[summary]})
+                    else:
+                        data_dict[postid]["summaries"].append(summary)
         
         return data_dict
     
@@ -52,7 +49,8 @@ class WebGPT:
 
     def __init__(self, split:str="train"):
         super().__init__()
-        dataset = load_dataset(self.name, split=split)
+        self.split = split
+        dataset = load_dataset(self.name, split=self.split)
         self.dataset_dict = defaultdict(dict)
         for item in dataset:
             post_id = item["question"]["id"]
@@ -82,56 +80,7 @@ class WebGPT:
         return question, answers
     
 
-@dataclass
-class RMDataCollator:
-    tokenizer:PreTrainedTokenizer
-    max_length:int=512
 
-
-    def format_example(self, example,eos,prompt=False):
-        sp_token = SPECIAL_TOKENS["prompter"] if prompt else SPECIAL_TOKENS["assistant"]
-        return "{}{}{}".format(sp_token,example,eos)
-
-    def process_example(self,example):
-
-        trunc_len = 0
-        eos = self.tokenizer.eos_token
-        prefix,outputs = example
-        prefix = self.format_example(example,eos,prompt=True)
-        outputs = [self.format_example(output,eos) for output in outputs]
-
-        prefix_tokens = self.tokenizer.encode(prefix)
-        input_ids, attention_masks = [],[]
-        for output in outputs:
-            out_tokens = self.tokenizer.encode(output,)
-            if len(prefix_tokens) + len(out_tokens) > self.max_length:
-                trunc_len = max(0,len(prefix_tokens) + len(out_tokens) - self.max_length)
-            prefix_tokens = prefix_tokens[trunc_len:]
-            out_tokens = prefix_tokens + out_tokens
-            out_tokens = out_tokens[:self.max_length]
-            pad_len = self.max_length - len(out_tokens)
-            attn_masks = [1] * len(out_tokens) + [0] * pad_len
-            out_tokens += [self.tokenizer.pad_token_id] * pad_len
-            input_ids.append(out_tokens)
-            attention_masks.append(attn_masks)
-        return input_ids, attention_masks
-    
-    def __call__(self,examples):
-        
-        batch_k_lens = [0]
-        input_ids, attention_masks = [],[]
-        for i,example in enumerate(examples):
-            inp_ids,attn_masks = self.process_example(example)
-            input_ids.extend(inp_ids)
-            attention_masks.extend(attn_masks)
-            batch_k_lens.append(batch_k_lens[i]+len(inp_ids))
-
-        return {
-            "input_ids":torch.tensor(input_ids),
-            "attention_mask":torch.tensor(attention_masks),
-            "k_lens":batch_k_lens
-        }
-            
 
 
         
