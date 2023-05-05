1b607604995d0a6751564b08d2a2793b5a21f196
Shahules786 <Shahules786@gmail.com>
2023-05-05 18:36:47 +0530

    black formatting

--- src/datacollator.py
+++ src/datacollator.py
@@ -7,51 +7,51 @@ from utils import SPECIAL_TOKENS
 
 @dataclass
 class RMDataCollator:
-    tokenizer:PreTrainedTokenizer
-    max_length:int=512
+    tokenizer: PreTrainedTokenizer
+    max_length: int = 512
 
-
-    def format_example(self, example,eos,prompt=False):
+    def format_example(self, example, eos, prompt=False):
         sp_token = SPECIAL_TOKENS["prompter"] if prompt else SPECIAL_TOKENS["assistant"]
-        return "{}{}{}".format(sp_token,example,eos)
+        return "{}{}{}".format(sp_token, example, eos)
 
-    def process_example(self,example):
-
+    def process_example(self, example):
         trunc_len = 0
         eos = self.tokenizer.eos_token
-        prefix,outputs = example
-        prefix = self.format_example(example,eos,prompt=True)
-        outputs = [self.format_example(output,eos) for output in outputs]
+        prefix, outputs = example
+        prefix = self.format_example(example, eos, prompt=True)
+        outputs = [self.format_example(output, eos) for output in outputs]
 
         prefix_tokens = self.tokenizer.encode(prefix)
-        input_ids, attention_masks = [],[]
+        input_ids, attention_masks = [], []
         for output in outputs:
-            out_tokens = self.tokenizer.encode(output,)
+            out_tokens = self.tokenizer.encode(
+                output,
+            )
             if len(prefix_tokens) + len(out_tokens) > self.max_length:
-                trunc_len = max(0,len(prefix_tokens) + len(out_tokens) - self.max_length)
+                trunc_len = max(
+                    0, len(prefix_tokens) + len(out_tokens) - self.max_length
+                )
             prefix_tokens = prefix_tokens[trunc_len:]
             out_tokens = prefix_tokens + out_tokens
-            out_tokens = out_tokens[:self.max_length]
+            out_tokens = out_tokens[: self.max_length]
             pad_len = self.max_length - len(out_tokens)
             attn_masks = [1] * len(out_tokens) + [0] * pad_len
             out_tokens += [self.tokenizer.pad_token_id] * pad_len
             input_ids.append(out_tokens)
             attention_masks.append(attn_masks)
         return input_ids, attention_masks
-    
-    def __call__(self,examples):
-        
+
+    def __call__(self, examples):
         batch_k_lens = [0]
-        input_ids, attention_masks = [],[]
-        for i,example in enumerate(examples):
-            inp_ids,attn_masks = self.process_example(example)
+        input_ids, attention_masks = [], []
+        for i, example in enumerate(examples):
+            inp_ids, attn_masks = self.process_example(example)
             input_ids.extend(inp_ids)
             attention_masks.extend(attn_masks)
-            batch_k_lens.append(batch_k_lens[i]+len(inp_ids))
+            batch_k_lens.append(batch_k_lens[i] + len(inp_ids))
 
         return {
-            "input_ids":torch.tensor(input_ids),
-            "attention_mask":torch.tensor(attention_masks),
-            "k_lens":batch_k_lens
+            "input_ids": torch.tensor(input_ids),
+            "attention_mask": torch.tensor(attention_masks),
+            "k_lens": batch_k_lens,
         }
-            
--- src/model.py
+++ src/model.py
@@ -1,6 +1,11 @@
 from turtle import forward
 from dataclasses import dataclass
-from transformers import GPTNeoXConfig, GPTNeoXPreTrainedModel, GPTNeoXForCausalLM, GPTNeoXModel
+from transformers import (
+    GPTNeoXConfig,
+    GPTNeoXPreTrainedModel,
+    GPTNeoXForCausalLM,
+    GPTNeoXModel,
+)
 from torch import nn
 import torch
 from transformers.utils import ModelOutput
@@ -8,30 +13,35 @@ from transformers.utils import ModelOutput
 
 @dataclass
 class GPTNeoxRMOuptput(ModelOutput):
-        """
-        Reward Model Output
-        """
-        logits: torch.FloatTensor = None
+    """
+    Reward Model Output
+    """
+
+    logits: torch.FloatTensor = None
+
 
 class GPTNeoXRM(GPTNeoXPreTrainedModel):
-    """
-    """
+    """ """
+
     def __init__(
-            self,
-            config,
+        self,
+        config,
     ):
         super().__init__(config)
         self.gpt_neox = GPTNeoXModel(config)
         self.out_layer = nn.Linear(config.hidden_size, 1)
 
     def forward(
-            self,
-            input_ids,
-            attention_mask,
-            **kwargs,
-
+        self,
+        input_ids,
+        attention_mask,
+        **kwargs,
     ):
-        return_dict = kwargs.get("return_dict") if kwargs.get("return_dict") is not None else self.config.use_return_dict
+        return_dict = (
+            kwargs.get("return_dict")
+            if kwargs.get("return_dict") is not None
+            else self.config.use_return_dict
+        )
         outputs = self.gpt_neox(
             input_ids,
             attention_mask,
@@ -40,17 +50,14 @@ class GPTNeoXRM(GPTNeoXPreTrainedModel):
         )
         hidden_states = outputs[0]
         if attention_mask is None:
-             hidden_states = hidden_states.mean(dim=1)
+            hidden_states = hidden_states.mean(dim=1)
         else:
-             hidden_states = (hidden_states * attention_mask.unsqueeze(-1)).sum(dim=1) / attention_mask.sum(dim=1).unsqueeze(-1)
+            hidden_states = (hidden_states * attention_mask.unsqueeze(-1)).sum(
+                dim=1
+            ) / attention_mask.sum(dim=1).unsqueeze(-1)
         lm_logits = self.out_layer(hidden_states)
 
         if not return_dict:
             return (lm_logits,) + outputs[1:]
-        
-        return GPTNeoxRMOuptput(
-             logits=lm_logits
-        )    
 
- 
-
+        return GPTNeoxRMOuptput(logits=lm_logits)
